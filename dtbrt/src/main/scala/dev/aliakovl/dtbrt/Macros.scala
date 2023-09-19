d9e57d5a0d33da7c950422463f861134110b6334
Яковлев Алексей Сергеевич <alek.yakovlev@tinkoff.ru>
2023-09-19 19:55:47 +0300

    dtbrt-end

--- dtbrt/src/main/scala/dev/aliakovl/dtbrt/Macros.scala
+++ dtbrt/src/main/scala/dev/aliakovl/dtbrt/Macros.scala
@@ -1,6 +1,8 @@
 package dev.aliakovl.dtbrt
 
 import internal.Into
+
+import scala.annotation.tailrec
 import scala.quoted.*
 
 object internal:
@@ -11,31 +13,35 @@ extension[From] (self: From)
 
 extension [From, To](self: Into[From, To])
   def withConstant[Field](selector: To => Field, value: Field): Into[From, To] = ???
+  def withComputed[Field](selector: To => Field, f: From => Field): Into[From, To] = ???
 
 extension [From, To](inline self: Into[From, To])
   inline def transform: To = ${ transformImpl[From, To]('self) }
 
 def transformImpl[From: Type, To: Type](expr: Expr[Into[From, To]])(using quotes: Quotes): Expr[To] =
   import quotes.reflect.*
-
   val (fromExpr, transformations) = extractTransformation(expr)
-  report.throwError(s"${fromExpr.show}, \n${transformations}")
+  val fromFieldsMap = getFields[From].map(f => f.name -> f).toMap
+  val toFields = getFields[To]
+  val transformationMap =
+    transformations.map(t => t.symbol.name -> t.makeExpr(fromExpr)).toMap
+  val args = toFields.map { field =>
+    transformationMap.getOrElse(field.name, makeFallback(fromExpr, field, fromFieldsMap))
+  }
+  construct[To](args)
 
-  expr match
-    case '{ dev.aliakovl.dtbrt.into[From]($fromExpr)[To] } =>
-      val fromFieldsMap = getFields[From].map(f => f.name -> f).toMap
-      val projections = getFields[To].map { toField =>
-        val field = fromFieldsMap.getOrElse(
-          toField.name,
-          report.throwError(s"I don't know what to do about `${toField.name}`")
-        )
-        projectField(fromExpr, field)
-      }
-      construct[To](projections)
-    case '{ dev.aliakovl.dtbrt.withConstant[From, To]($fromExpr)[field]($selectorExpr, $valueExpr) } =>
-      report.throwError(s"${fromExpr.show}")
-//    case qwer => report.throwError(s"${ qwer.show }")
-    case _ => report.throwError("OH NO!")
+def makeFallback[From: Type](using quotes: Quotes)(
+    fromExpr: Expr[From],
+    toField: quotes.reflect.Symbol,
+    fromFieldMap: Map[String, quotes.reflect.Symbol]
+): Expr[Any] =
+  import quotes.reflect.*
+  val field = fromFieldMap.getOrElse(
+    toField.name,
+    report.throwError(s"I don't know what to do about `${toField.name}`")
+  )
+  projectField(fromExpr, field)
+
 
 def getFields[A: Type](using quotes: Quotes): List[quotes.reflect.Symbol] =
   import quotes.reflect.*
@@ -58,30 +64,36 @@ def construct[A: Type](args: List[Expr[Any]])(using quotes: Quotes): Expr[A] =
   )
   result.asExprOf[A]
 
-class Transformation[Q <: Quotes](using val quotes: Q)(
+class Transformation[From: Type, Q <: Quotes](using val quotes: Q)(
   val symbol: quotes.reflect.Symbol,
-  val expr: Expr[Any]
-)
+  val makeExpr: Expr[From] => Expr[Any]
+):
+  override def toString: String = s"Transformation(${symbol.name}, ${makeExpr})"
 
 def extractTransformation[From: Type, To: Type](
   expr: Expr[Into[From, To]]
-)(using quotes: Quotes): (Expr[From], List[Transformation[quotes.type]]) =
+)(using quotes: Quotes): (Expr[From], List[Transformation[From, quotes.type]]) =
   import quotes.reflect.*
+  @tailrec
   def loop(
     expr: Expr[Into[From, To]],
-    acc: List[Transformation[quotes.type]]
-  ): (Expr[From], List[Transformation[quotes.type]]) = expr match
+    acc: List[Transformation[From, quotes.type]]
+  ): (Expr[From], List[Transformation[From, quotes.type]]) = expr match
     case '{ dev.aliakovl.dtbrt.into[From]($fromExpr)[To] } => fromExpr -> acc
     case '{ dev.aliakovl.dtbrt.withConstant[From, To]($nested)[field]($selector, $value) } =>
       val symbol: Symbol = selector.asTerm match
         case Lambda(_, select @ Select(_, _)) => select.symbol
-      val transformation = Transformation[quotes.type]()(
+        case term => report.throwError("only projection is available", term.pos)
+      val transformation = Transformation[From, quotes.type]()(symbol, _ => value)
+      loop(nested, transformation :: acc)
+    case '{ dev.aliakovl.dtbrt.withComputed[From, To]($nested)[field]($selector, $f) } =>
+      val symbol: Symbol = selector.asTerm match
+        case Lambda(_, select@Select(_, _)) => select.symbol
+        case term => report.throwError("only projection is available", term.pos)
+      val transformation = Transformation[From, quotes.type]()(
         symbol,
-        value
+        fromExpr => Expr.betaReduce('{ $f($fromExpr) })
       )
-      loop(
-        nested,
-        transformation :: acc
-      )
-
+      loop(nested, transformation :: acc)
+    case _ => report.throwError("error")
   loop(expr, List.empty)
--- dtbrt/src/main/scala/dev/aliakovl/dtbrt/Main.scala
+++ dtbrt/src/main/scala/dev/aliakovl/dtbrt/Main.scala
@@ -1,17 +1,18 @@
 package dev.aliakovl.dtbrt
 
-case class Person(name: String, age: Int, isAlive: Boolean, other: Double = 5.5)
+case class Person(name: String, age: Int, isAlive: Boolean, dullness: Double)
 case class User(name: String, isAlive: Boolean, age: Int, fanciness: Double)
 
-def notifyUser(user: User): Unit = println(s"Hey, ${user.name} (${user.age} years old) (alive: ${user.isAlive})!")
+def notifyUser(user: User): Unit = println(s"Hey, ${user.name} (${user.age} years old) (alive: ${user.isAlive}) (fanciness: ${user.fanciness}) !")
 
 def person2User(person: Person): User =
   person.into[User]
-    .withConstant(_.fanciness, 99.9)
+    .withConstant(_.age, 6)
+    .withComputed(_.fanciness, _.dullness * 10)
     .transform
 
 @main
 def run(): Unit =
-  val person: Person = Person("Aleksei", 25, true)
+  val person: Person = Person("Aleksei", 25, true, 55)
   val user: User = person2User(person)
   notifyUser(user)
