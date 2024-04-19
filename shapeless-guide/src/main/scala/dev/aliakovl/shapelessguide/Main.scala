105e8e19cf84b69ebfe006736880dc2b2aa8859e
Aleksei Iakovlev <mister.yakowlev2011@gmail.com>
2024-04-19 10:16:58 +0300

    shapeless-guide

--- shapeless-guide/src/main/scala/dev/aliakovl/shapelessguide/Main.scala
+++ shapeless-guide/src/main/scala/dev/aliakovl/shapelessguide/Main.scala
@@ -3,12 +3,15 @@ package dev.aliakovl.shapelessguide
 import cats.Monoid
 import dev.aliakovl.shapelessguide.Migration.MigrationOps
 import dev.aliakovl.shapelessguide.ProductMapper.ProductMapperOps
+import dev.aliakovl.shapelessguide.SizeOf.sizeOf
 import dev.aliakovl.shapelessguide.examples.CsvEncoder.writeCsv
+import dev.aliakovl.shapelessguide.examples.Random._
 import dev.aliakovl.shapelessguide.examples._
 import dev.aliakovl.shapelessguide.examples.json.JsonEncoder
 import shapeless._
 import shapeless.labelled.{FieldType, field}
 import shapeless.ops.hlist.Last
+import shapeless.ops.nat.ToInt
 import shapeless.record._
 import shapeless.syntax.singleton.mkSingletonOps
 
@@ -29,6 +32,13 @@ case class IceCreamV2c(
     numCherries: Int,
     numWaffles: Int
 )
+
+case class Cell(col: Char, row: Int)
+
+sealed trait Light
+case object Red extends Light
+case object Amber extends Light
+case object Green extends Light
 
 object Main {
   def getFieldName[K, V](value: FieldType[K, V])(implicit
@@ -64,7 +74,7 @@ object Main {
 
     println(CsvEncoder[Foo].encode(Foo(Bar(3, "wef"))))
 
-    println(reify { CsvEncoder[Double] })
+//    println(reify { CsvEncoder[Double] })
 
     val a = implicitly[Last[String :: Int :: HNil]].apply("abc" :: 123 :: HNil)
 
@@ -141,6 +151,16 @@ object Main {
 
     println(IceCream1("Sundae", 1, false).mapTo[IceCream2](conversions))
 
+    val y = ToInt[Succ[Succ[_0]]].apply()
+
+    println(sizeOf[IceCream])
+
+    for (i <- 1 to 3) println(random[Char])
+    for (i <- 1 to 3) println(random[Int])
+
+    for (i <- 1 to 5) println(random[Cell])
+
+    for (i <- 1 to 5) println(random[Light])
   }
 
   object myPoly extends Poly1 {
@@ -152,11 +172,10 @@ object Main {
   }
 
   object conversions extends Poly1 {
-    implicit val intCase:  Case.Aux[Int, Boolean]   = at(_ > 0)
-    implicit val boolCase: Case.Aux[Boolean, Int]   = at(if(_) 1 else 0)
-    implicit val strCase:  Case.Aux[String, String] = at(identity)
+    implicit val intCase: Case.Aux[Int, Boolean] = at(_ > 0)
+    implicit val boolCase: Case.Aux[Boolean, Int] = at(if (_) 1 else 0)
+    implicit val strCase: Case.Aux[String, String] = at(identity)
   }
-
 
   case class IceCream1(name: String, numCherries: Int, inCone: Boolean)
   case class IceCream2(name: String, hasCherries: Boolean, numCones: Int)
