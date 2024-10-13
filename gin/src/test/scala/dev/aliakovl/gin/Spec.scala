7ddd5b9a61c956365d3b0ed6e20710460d863e98
Aleksei Iakovlev <mister.yakowlev2011@gmail.com>
2024-10-13 12:05:11 +0300

    gin-tests

--- gin/src/test/scala/dev/aliakovl/gin/Spec.scala
+++ gin/src/test/scala/dev/aliakovl/gin/Spec.scala
@@ -1,14 +1,44 @@
 package dev.aliakovl.gin
 
-import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
-class Spec extends AnyFlatSpec with Matchers {
-  "tuple" should "compile" in {
-    "Gen.custom[(Int, String)].make" should compile
-  }
+class Spec extends AnyWordSpec with Matchers {
+  "Gen.custom" should {
+    "not compile" when {
+      "trait is not sealed" in {
+        trait A
+        final class B extends A
+        "Gen.custom[A].make" shouldNot compile
+        "Gen.custom[Option[A]].make" shouldNot compile
+      }
 
-  "int" should "not compile" in {
-    "Gen.custom[Int].make" shouldNot compile
+      "abstract class is not sealed" in {
+        abstract class A
+        final class B extends A
+        "Gen.custom[A].make" shouldNot compile
+        "Gen.custom[Option[A]].make" shouldNot compile
+      }
+
+      "sealed trait does not have child classes" in {
+        sealed trait A
+        "Gen.custom[A].make" shouldNot compile
+        "Gen.custom[Option[A]].make" shouldNot compile
+      }
+
+      "sealed abstract class does not have child classes" in {
+        sealed abstract class A
+        "Gen.custom[A].make" shouldNot compile
+        "Gen.custom[Option[A]].make" shouldNot compile
+      }
+
+      "child class is neither final nor a case class" in {
+        sealed trait A
+        class B extends A
+        "Gen.custom[A].make" shouldNot compile
+        "Gen.custom[Option[A]].make" shouldNot compile
+      }
+
+    }
   }
 }
